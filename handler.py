0268fd68af35fb63e690dd820df748e6ce655a4d
Nicholas Griffin <nicholas.griffin@accrosoft.com>
2019-05-20 01:04:02 +0100

    Adding app

--- handler.py
+++ handler.py
@@ -1,13 +1,23 @@
-import os, boto3
+import os, boto3, json
 
 NEGATIVE_COMMENT_MESSAGE="Your comment is too negative to post, please edit it."
 POSITIVE_COMMENT_MESSAGE="Your comment looks great. Keep up the good work!"
 
 client = boto3.client('comprehend')
 
-def lambda_handler(event, context):
-    sentiment=client.detect_sentiment(Text=event['inputTranscript'],LanguageCode='en')['Sentiment']
+def sentimentAnalysis(event, context):
+    
+    inputTranscriptData = event['queryStringParameters']['inputTranscript']
+    print(inputTranscriptData);
+    
+    sentiment=client.detect_sentiment(Text=inputTranscriptData,LanguageCode='en')['Sentiment']
     if sentiment=='NEGATIVE':
-        return NEGATIVE_COMMENT_MESSAGE;
+        return {
+            "statusCode": 200,
+            "body": json.dumps(NEGATIVE_COMMENT_MESSAGE)
+        }
     else:
-        return POSITIVE_COMMENT_MESSAGE;
+        return {
+            "statusCode": 200,
+            "body": json.dumps(POSITIVE_COMMENT_MESSAGE)
+        }
